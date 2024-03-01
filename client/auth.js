74c27136b657272f41b30e241d0d95c567b27c08
Beatriz Bravo <beatz.bravo@gmail.com>
2024-03-01 12:10:02 -0700

    generateRandom code verifier for spotify PKCE authorization

---
+++ client/auth.js
@@ -0,0 +1,14 @@
+// PKCE authorization 
+
+// will need a code verifier, which is a random string between 43 and 128 characters
+
+const generateRandomString = (length) => {
+    const possibleCharacters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789'
+    // console.log(possibleCharacters)
+    const values = crypto.getRandomValues(new Uint8Array(length));
+    return values.reduce((acc, x) => acc + possibleCharacters[x % possibleCharacters.length], "")
+}
+
+const codeVerifier = generateRandomString(64);
+
+// need to hash the codeVerifier 
